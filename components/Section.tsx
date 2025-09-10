c2ebec8bd95965233a35a9f7c726ef41426781a2
mydsrt-artweb <my.project944@gmail.com>
2025-09-11 04:28:06 +0700

    Update Section.tsx

--- components/Section.tsx
+++ components/Section.tsx
@@ -13,7 +13,7 @@ export default function Section({ title, children }: SectionProps) {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ once: true }}
-      className="max-w-3xl mx-auto my-8 p-6 
+      className="max-w-6xl mx-auto my-8 p-6 
                  bg-white/70 backdrop-blur-md 
                  border border-gray-300/50 
                  text-center rounded-2xl shadow-lg
